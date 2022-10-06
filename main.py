cfad0f9bdc2fa28bae31111da51ef967ee99a970
Mateus Dias Oliveira <dias.mateus@academico.ifrn.edu.br>
2022-10-06 09:20:32 -0300

    Segundo commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 ## CONFIGURAÇÕES
 
-TAMANHO = 64
+TAMANHO = 100
 ALTURA = (0, 10)
 
 ##################
@@ -13,13 +13,15 @@ listaX = []
 listaY = []
 
 for i in range(TAMANHO):
-    if listaY == "SUBSTITUI!!":
+    if randint(0,100) <= 80 and len(listaY) > 0:
+        y = listaY[-1]
+    elif randint(0,100) <= 90:
         pass
-    y = randint(ALTURA[0], ALTURA[1])
+    else:
+        y = randint(ALTURA[0], ALTURA[1])
     listaX.append(i)
     listaY.append(y)
     print(y)
-
 
 plt.plot(listaX, listaY)
 plt.show()
